119f642432b29e5dd362eb82432a53ac5999021e
dat <snapeandcandy@gmail.com>
2025-04-21 22:32:37 +0700

    nvim: disable recording to register q

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_set_keymap("n", "q", "<Nop>", {
+  noremap = true,
+  slient = true,
+  desc = "Disable recording macro to register q",
+})
